d1e46d8da68d7ccf3ddb7c7445663402b6176b9d
itsdatnguyen <itsdatnguyen@gmail.com>
2017-07-17 00:28:17 -0400

    Optimized images, removed unneeded dependancies and added css improvements

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 (function () {
+    // google analytics
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-101860652-1', 'auto');
+    ga('send', 'pageview');
+
     var lgBreakpoint = 1200;
 
     var parallaxImageSelector = ".parallax-image";
@@ -11,8 +21,6 @@
     };
 
     function updateParallax() {
-        updateImageSize();
-
         // disable stellar for devices under 1200px wide
         if ($(window).width() < lgBreakpoint) {
             $(window).stellar("destroy");
@@ -29,12 +37,6 @@
                 verticalOffset: 100
             });
         }
-    }
-
-    function updateImageSize() {
-        // resize parallax images to the height of the viewport
-        var height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        $(parallaxImageSelector).css("height", height);
     }
 
     $(window).load(function () {
